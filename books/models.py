b77c1d5e01d721ccd11eb2ea9d16c2e7c3b6b22a
Tom Booster <tom@Toms-MacBook-Pro.local>
2019-11-05 13:33:11 -0800

    Chapter 11: Reviews App

--- books/models.py
+++ books/models.py
@@ -1,10 +1,11 @@
-import uuid 
+import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 
 class Book(models.Model):
-    id = models.UUIDField( 
+    id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False)
@@ -16,4 +17,20 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
+        return reverse('book_detail', kwargs={'pk': str(self.pk)})
+
+
+class Review(models.Model): 
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
