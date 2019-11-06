bb55bf845101012206fd295b97ce41a74b4a3756
Tom Booster <tom@Toms-MacBook-Pro.local>
2019-11-06 08:16:38 -0800

    Chapter 12: File/Image Uploads

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -137,6 +137,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # django-crispy-forms
